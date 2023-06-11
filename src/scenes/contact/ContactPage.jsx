1030d9d73f25934d182fc71993dfa46c1bc42e12
jaiho <jaihind@gmail.code>
2023-06-11 06:01:27 +0530

    added box

--- src/scenes/contact/ContactPage.jsx
+++ src/scenes/contact/ContactPage.jsx
@@ -8,7 +8,7 @@ function ContactPage(){
         <div className="h-10 w-full flex justify-center items-center bg-blue-700">
           <h2 className="text-xl font-normal">Create Page</h2>
         </div>
-        <div className="flex justify-around items-center flex-col border-blue-700 border-[8px] min-h-[500px]git w-full">
+        <div className="flex justify-around items-center flex-col border-blue-700 border-[8px] min-h-[500px] w-full">
               <div>
                 <button className="bg-slate-400 pt-2 pb-2 pl-4 pr-4">
                   Create Contact
