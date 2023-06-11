512a3e9f782235496fcdb0ce21744124e0e69f5f
jaiho <jaihind@gmail.code>
2023-06-12 01:37:56 +0530

    added liflet with functionality

--- src/scenes/map&chart/MapAndChartPage.jsx
+++ src/scenes/map&chart/MapAndChartPage.jsx
@@ -1,15 +1,15 @@
 
 import React from 'react'
 import LeafletMap from './map/LeafletMap'
-// import Chart from './chart/Chart'
-// import LineChart from './chart/LineChart'
+import LineChart from './chart/LineChart'
 
 function MapAndChartPage() {
   return (
-    <div className='min-h-screen min-w-full flex justify-center items-center'>
+    <div className='min-h-screen min-w-full flex justify-center items-center flex-col'>
       <div className=' min-h-[500px] min-w-full flex justify-center items-center'>
-          {/* <Chart/> */}
-          {/* <LineChart/> */}
+          <LineChart/>
+      </div>
+      <div className=' min-h-[500px] sm:w-5/6 flex justify-center items-center sm:bg-slate-400 sm:mt-10 pt-10 pb-10 sm:ml-10 mb-10 p-6'>
           <LeafletMap/>
       </div>
     </div>
--- src/scenes/map&chart/map/LeafletMap.jsx
+++ src/scenes/map&chart/map/LeafletMap.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {MapContainer as Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
 
 
 
@@ -12,16 +14,19 @@ const MapComponent = () => {
         .then((res)=>{setCountriesData([...res])});
     },[])
 
+    const customIcon= new Icon({
+        iconUrl:"https://cdn-icons-png.flaticon.com/512/819/819814.png",
+        iconSize:[30,30]
+    })
 
   return (
-    <div className='w-3/5 h-4/5'>
-        <Map center={[0, 0]} zoom={2}>
+    <Map center={[0, 0]} zoom={2} className='z-10 sm:h-[450px] w-full sm:bg-slate-400 text-red-50'>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {countriesData.map((country) => (
-        <Marker position={[country.countryInfo.lat, country.countryInfo.long]} key={country.name}>
+        <Marker position={[country.countryInfo.lat, country.countryInfo.long]} icon={customIcon} key={country.name}>
           <Popup>
             <div>
-              <h2>{country.country}</h2>
+              <h2>Country : { country.country}</h2>
               <p>Total Active Cases: {country.active}</p>
               <p>Total Recovered Cases: {country.recovered}</p>
               <p>Total Deaths: {country.deaths}</p>
@@ -30,7 +35,7 @@ const MapComponent = () => {
         </Marker>
       ))}
     </Map>
-    </div>
+    // </div>
   );
 };
 
