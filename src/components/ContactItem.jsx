7976e3e43a7800bda839ee543a972566cdc42f2d
jaiho <jaihind@gmail.code>
2023-06-12 08:31:15 +0530

    added img in contactlis

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -13,7 +13,7 @@ function ContactItem({id,firstname,lastname,status}) {
             <div className='w-[75px] h-[75px] rounded-[50%] flex justify-center items-center bg-slate-400 '>
               <img src='https://cdn-icons-png.flaticon.com/512/3459/3459528.png' width={55} alt='logo'/><img  alt=''/>
             </div>
-            <div className='flex flex-col gap-2 text-[16px]'>
+            <div className='flex flex-col gap-2 text-[14px]'>
               <span>First Name: {firstname}</span>
               <span>Last Name: {lastname}</span>
               <span>Status: {status}</span>
